3e19c13ae8dcee9b17e5328a204e86746e5dcb9e
dreese33 <dylanjacobreese@gatech.edu>
2020-08-26 11:48:20 -0400

    Added multiple characters created with Blender and Makehuman. Animations were also created with Blender. This commit includes an animated zombie character (zombie walk towards player), female/male characters (idle, walk, run animations). Scripting added as well for camera movement (rotation by touch and drag of finger for 3d world). Also added scripting for basic player animation state machine.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    private Touch touch;
+
+    public float rotateSpeed = 3.0f;
+    public int invertPitch = 1;
+
+    private float pitch, yaw = 0.0f;
+    public readonly Vector3 offset = new Vector3(0.0f, 17.5f, 3.0f);
+
+    public bool PlayerAlive = true;
+    public Transform target;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateCameraPosition();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerAlive == true)
+        {
+            if (Input.touchCount > 0)
+            {
+                touch = Input.GetTouch(0);
+                if (touch.phase == TouchPhase.Moved) 
+                {
+                    UpdateCameraAngle();
+                }
+            }
+        }
+    }
+
+    void UpdateCameraAngle()
+    {
+        pitch -= touch.deltaPosition.y * rotateSpeed * invertPitch * Time.deltaTime;
+        yaw += touch.deltaPosition.x * rotateSpeed * invertPitch * Time.deltaTime;
+
+        pitch = Mathf.Clamp(pitch, -60, 60);
+        yaw = Mathf.Clamp(yaw, -60, 60);
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+
+
+    //This is currently setup for the female character only
+    //This code should be executed in the Player class
+    void UpdateCameraPosition()
+    {
+        transform.position = target.position + offset;
+        //transform.position = offset;
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    enum CurrentAnimationState { IDLE, WALK, RUN };
+
+    private Animation anim;
+    private CameraController controller;
+    private CurrentAnimationState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animation>();
+        controller = GameObject.Find("Main Camera").GetComponent<CameraController>();
+        state = CurrentAnimationState.IDLE;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller.PlayerAlive == true) 
+        {
+            if (anim.isPlaying) {
+                return;
+            }
+
+            switch (state)
+            {
+                case CurrentAnimationState.IDLE:
+                    gameObject.GetComponent<Animation>().Play("Female|Run");
+                    //anim.Play("Female|Idle");
+                    break;
+                case CurrentAnimationState.WALK:
+                    anim.Play("Walk");
+                    break;
+                case CurrentAnimationState.RUN:
+                    anim.Play("Run");
+                    break;
+            }
+        }
+    }
+}
