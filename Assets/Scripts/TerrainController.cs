5d9e38004d77765a4c27aaf1c4a9fd3490779e28
dreese33 <dylanjacobreese@gatech.edu>
2020-08-31 11:41:01 -0400

    Code cleanup in TreeController

--- Assets/Scripts/TerrainController.cs
+++ Assets/Scripts/TerrainController.cs
@@ -5,19 +5,17 @@ using UnityEngine;
 public class TerrainController : MonoBehaviour
 {
 
-    //To fix bug tomorrow try to use:
+    //Add texture to Terrain
     //https://answers.unity.com/questions/538769/change-and-add-terrain-texture-during-runtime.html
 
 
     public Terrain terrain;
-    private Terrain frontTerrain;
     private static Terrain terrainObj;
 
     //The total number of terrain objects rendered
     public int terrainCount = 0;
     private float terrainLength;
     private PlayerController player;
-    private bool lastSelected = false;
     private static bool firstIter = true;
 
     // Start is called before the first frame update
@@ -46,18 +44,8 @@ public class TerrainController : MonoBehaviour
     {
         Debug.Log("Add new terrain");
         terrainCount++;
-        if (lastSelected) {
-            lastSelected = false;
-        } else {
-            lastSelected = true;
-        }
 
         CreateNewTerrain();
-
-        /*if (terrainCount > 2)
-        {
-            DestroyTerrain();
-        }*/
     }
 
 
@@ -70,43 +58,7 @@ public class TerrainController : MonoBehaviour
             terrainObj.name = "TerrainObj" + terrainCount;
             Debug.Log("Another one created");
         } else
-        {/*
-            if (lastSelected)
-            {
-                //Move terrain to front
-                Debug.Log("Try");
-                Vector3 terrainPos = terrain.transform.position;
-                Debug.Log(terrainPos.z);
-                Debug.Log(terrainObj.transform.position.z);
-                if (terrainCount % 2 != 0)
-                {
-                    terrainPos.z = terrainCount * terrainLength;
-                    terrain.transform.position = terrainPos;
-                    Debug.Log("Last selected");
-                } else if (terrainCount == 2)
-                {
-                    Debug.Log("Error prone zone");
-                    terrainPos.z = terrainCount * terrainLength;
-                }
-            } else
-            {
-                //Move terrainObj to front
-                //if (terrainObj != null)
-                //{
-                    Debug.Log("Try2");
-                    Vector3 terrainPos = terrainObj.transform.position;
-                    Debug.Log(terrainPos.z);
-                    Debug.Log(terrain.transform.position.z);
-                    Debug.Log("COunt" + terrainCount);
-                    if (terrainCount % 2 == 0 && terrainCount != 2)
-                    {
-                        terrainPos.z = terrainCount * terrainLength;
-                        Debug.Log("Else");
-                    }
-
-                    terrainObj.transform.position = terrainPos;
-                //}*/
-
+        {
             if (terrainCount % 2 == 0)
             {
                 Debug.Log("Moved terrain");
@@ -123,27 +75,6 @@ public class TerrainController : MonoBehaviour
                 terrainNow.transform.position = terrainPos;
             }
         }
-        //Set front terrain to terrain
-        /*
-        terrainObj = new GameObject("TerrainObj" + terrainCount);
- 
-        TerrainData terrainData = new TerrainData();
-        terrainData.size = new Vector3(100, 600, 1000);
-        
-        //TerrainLayer[] terrainLayers = new TerrainLayer[1];
-
-        terrainData.terrainLayers = terrain.terrainData.terrainLayers;
-
-        TerrainCollider terrainCollider = terrainObj.AddComponent<TerrainCollider>();
-        Terrain newTerrain = terrainObj.AddComponent<Terrain>();
-        
-        terrainCollider.terrainData = terrainData;
-        newTerrain.terrainData = terrainData;
-        frontTerrain = newTerrain;
-
-        Vector3 terrainPos = terrainObj.transform.position;
-        terrainPos.z = terrainCount * terrainLength;
-        terrainObj.transform.position = terrainPos;*/
     }
 
 
