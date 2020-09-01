a6c1df425833136d53fb22ee5a2a0d2af0974cf6
dreese33 <dylanjacobreese@gatech.edu>
2020-09-01 12:28:55 -0400

    Added multiple camera functionality for switching to the pause screen UI. Also restructured code

---
+++ Assets/Scripts/Controllers/PauseMenuController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuController : MonoBehaviour
+{
+
+    public Vector3 screenSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RectTransform rt = GetComponent<RectTransform>();
+        screenSize = Camera.main.ViewportToWorldPoint(Vector3.up + Vector3.right);
+
+        float sizeX = screenSize.x / rt.rect.width;
+        float sizeY = screenSize.y / rt.rect.height;
+        rt.localScale = new Vector3(sizeX * 0.8f, sizeY * 0.8f, 1);
+    }
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Button pauseButton;
+    public Button resumeButton;
+    public GameObject pauseMenuUI;
+    public bool gameIsPaused = false;
+    public Camera menuCamera;
+    public Camera mainCamera;
+    public GameObject character;
+
+    private float posZOffset = 15.0f;
+
+    void Start()
+    {
+        pauseButton.onClick.AddListener(Pause);
+        resumeButton.onClick.AddListener(Resume);
+        pauseMenuUI.SetActive(false);
+    }
+
+    
+    void Pause()
+    {
+        if (!gameIsPaused)
+        {
+            pauseMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            gameIsPaused = true;
+
+            UpdatePauseMenuLocation();
+
+            menuCamera.depth += 1;
+            mainCamera.GetComponent<AudioListener>().enabled = false;
+            menuCamera.GetComponent<AudioListener>().enabled = true;
+
+            menuCamera.transform.position = mainCamera.transform.position;
+            menuCamera.transform.rotation = mainCamera.transform.rotation;
+        }
+    }
+
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+
+        menuCamera.depth -= 1;
+        mainCamera.GetComponent<AudioListener>().enabled = true;
+        menuCamera.GetComponent<AudioListener>().enabled = false;
+    }
+
+
+    void UpdatePauseMenuLocation()
+    {
+        Vector3 pos = mainCamera.transform.position;
+        pos.z += posZOffset;
+        pauseMenuUI.transform.position = pos;
+    }
+}
