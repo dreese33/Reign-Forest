fc4ab27d35683d5bf5908b53db820792433a2724
dreese33 <dylanjacobreese@gatech.edu>
2020-09-21 18:14:17 -0400

    Created function for spawning multiple zombies

--- Assets/Scripts/GenerateEnemies.cs
+++ Assets/Scripts/GenerateEnemies.cs
@@ -6,6 +6,7 @@ public class GenerateEnemies : MonoBehaviour
 {
 
     public static float levelOfDifficulty = 1.0f;
+    private bool beingHandled = false;
 
     //Replace with pooling later
     public GameObject zombie;
@@ -13,7 +14,31 @@ public class GenerateEnemies : MonoBehaviour
     void Start()
     {
         Debug.Log("Working");
+
+
+        StartCoroutine(SpawnZombies(10));
     }
+
+
+    private IEnumerator SpawnZombies(int iters)
+    {
+        beingHandled = true;
+
+        for (int i = 0; i < iters; i++)
+        {
+            Instantiate(zombie);
+            yield return new WaitForSeconds(RandomNumberSeconds());
+        }
+
+        beingHandled = false;
+    }
+
+
+    private float RandomNumberSeconds()
+    {
+        return Random.Range(0.5f, 1.5f);
+    }
+
 
     void Update()
     {
